3b16579b7bde7cff10769e38e7d1bc3bbec06048
Alvin Yap <leaf.alvin@gmail.com>
2023-12-04 16:06:10 +0800

    submit: day04 part 1

---
+++ day04/part1/solutions.js
@@ -0,0 +1,78 @@
+/* Imports */
+const fs = require("fs");
+const readline = require("readline");
+
+/* Utilities / Helper Functions */
+function extractWinNums(string) {
+  let winNumTxt = string.split(":")[1];
+  //   console.log(winNumTxt);
+  let winNumArr = winNumTxt.split(" ");
+  let parsedWinNumArr = [];
+  for (let num of winNumArr) {
+    if (num !== "") parsedWinNumArr.push(num);
+  }
+  //   console.log(parsedWinNumArr);
+  return parsedWinNumArr;
+}
+
+function extractMyHand(string) {
+  let myNumArr = string.split(" ");
+  let parsedMyNumArr = [];
+  for (let num of myNumArr) {
+    if (num !== "") parsedMyNumArr.push(num);
+  }
+  //   console.log(parsedMyNumArr);
+  return parsedMyNumArr;
+}
+
+/* Sub functions */
+function processTxt(linesArray) {
+  // Total points
+  let totalPoints = 0;
+  for (let line of linesArray) {
+    let match = 0;
+    // Process every hand
+    let splittedText = line.split("|");
+    let winNum = extractWinNums(splittedText[0]);
+    let myNums = extractMyHand(splittedText[1]);
+
+    // console.log(winNum);
+    // console.log(myNums);
+
+    // Find matching
+    for (let num of myNums) {
+      if (winNum.includes(num)) match += 1;
+    }
+    console.log("Matches: ", match);
+    if (match != 0) totalPoints += 1 * 2 ** (match - 1);
+  }
+  console.log(totalPoints);
+}
+
+/* Main Function */
+async function main() {
+  /* Create readstream */
+  const fileStream = fs.createReadStream("input.txt");
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  // Line Array
+  let linesArray = [];
+
+  // Asynchronous
+  rl.on("line", (line) => {
+    linesArray.push(line);
+  });
+
+  // After read line event has finished
+  rl.on("close", () => {
+    for (let line of linesArray) console.log(line);
+    processTxt(linesArray);
+  });
+}
+
+main();
+
+// Answer => 23673
