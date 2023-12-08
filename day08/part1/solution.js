21b0e58bb1c5c84c4648ed49d583a58ef714138d
Alvin Yap <leaf.alvin@gmail.com>
2023-12-08 14:09:13 +0800

    submit: day08 part 1

--- day08/part1/solution.js
+++ day08/part1/solution.js
@@ -9,27 +9,62 @@
 const fs = require("fs");
 const readline = require("readline");
 
-/*** Utilities & Helper Functions */
-
 /***  Sub Logics ***/
+// Extract instructions into a single string
 function extractInstructions(linesArray) {
   return linesArray[0];
 }
 
-/*** Logics ***/
+// Extract map into an object
+function extractMap(linesArray) {
+  let mapContainer = {};
 
+  for (let i = 2; i < linesArray.length; i++) {
+    let splittedText = linesArray[i].split(" = ");
+    let key = splittedText[0];
+    let [left, right] = splittedText[1].split(", ");
+    left = left.replace("(", "");
+    right = right.replace(")", "");
+    mapContainer[key] = {};
+    mapContainer[key].left = left;
+    mapContainer[key].right = right;
+  }
+  return mapContainer;
+}
+
+function travel(instructions, map) {
+  let stepCounter = 0;
+  let instructionsLength = instructions.length;
+  let nextDest = "AAA";
+  while (nextDest != "ZZZ") {
+    let direction = instructions[stepCounter % instructionsLength]; // L or R
+    if (direction == "L") {
+      nextDest = map[nextDest].left;
+    } else if (direction == "R") {
+      nextDest = map[nextDest].right;
+    }
+    stepCounter++;
+  }
+  console.log("Steps: ", stepCounter);
+}
+
+/*** Logics ***/
 function processInput(linesArray) {
   // Extract Instructions
   let instructions = extractInstructions(linesArray);
-  console.log(instructions);
+  console.log("Instructions: \n", instructions);
   // Extract Map
+  let map = extractMap(linesArray);
+  console.log("Map: \n", map);
+  // Travel to end
+  travel(instructions, map);
 }
 
 /*** Main ***/
 
 async function main() {
   /* Create readstream */
-  const fileStream = fs.createReadStream("test_input1.txt");
+  const fileStream = fs.createReadStream("input.txt");
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
